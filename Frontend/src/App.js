52d1b240e077325cdc300c90e798e18ea19eac56
mwen6 <103011669+mwen6@users.noreply.github.com>
2022-05-07 13:12:48 +0800

    Add files via upload

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import AppIndex from './pages/AppIndex.js';
 import AppDetail from './pages/AppDetail.js';
-import Home from './pages/Home.js';
+import AppAbout from './pages/AppAbout.js';
 import { Routes, Route } from "react-router-dom";
 import SignUp from './components/signup';
 
@@ -9,10 +9,10 @@ function App() {
   return (
     <div className="app">
       <Routes>
+        <Route path="/" element={<AppIndex />} />
         <Route path="/detail/:id" element={<AppDetail />} />
         <Route path="/user/signup" element={<SignUp />} />
-        <Route path="/list" element={<AppIndex />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AppAbout />} />
       </Routes>
     </div>
   );
